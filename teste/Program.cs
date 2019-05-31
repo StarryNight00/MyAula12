00cd3188941b22b3bf3839b5c8fcd7bdabc2aa80
StarryNight00 <cisa.matias@gmail.com>
2019-05-31 13:02:12 +0100

    excs atualizados

--- teste/Program.cs
+++ teste/Program.cs
@@ -6,7 +6,16 @@ namespace teste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(3.CompareTo(2));
+            Console.WriteLine((3).CompareTo(3));
             Console.WriteLine("abc".CompareTo("df"));
+
+            Console.WriteLine();
+            Console.WriteLine("olá".GetHashCode());
+            Console.WriteLine("olé".GetHashCode());
+            Console.WriteLine("olá".GetHashCode());
+            Console.WriteLine("2".GetHashCode());
+            Console.WriteLine(2.GetHashCode());
 
             Console.ReadKey();
         }
